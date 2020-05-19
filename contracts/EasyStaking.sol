85d4f4cd74dc2943df07e8021abb5b55a6e8919d
Max Alekseenko <nnwen0609@gmail.com>
2020-05-19 20:48:16 +0300

    document the code

--- contracts/EasyStaking.sol
+++ contracts/EasyStaking.sol
@@ -7,30 +7,68 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/SafeERC20
 import "./IERC20Mintable.sol";
 import "./Sacrifice.sol";
 
+/**
+ * @title EasyStaking
+ *
+ * Note: all percentage values are between 0 and 1
+ * and represented as fixed point numbers with 18 decimals like in Ether
+ * 100% == 1 ether
+ */
 contract EasyStaking is Ownable {
     using Address for address;
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
+    /**
+     * @dev Emitted when a user deposits tokens
+     * @param sender User address
+     * @param amount The amount of deposited tokens
+     * @param customId Custom identifier (for exchanges)
+     */
     event Deposited(address indexed sender, uint256 amount, string customId);
+
+    /**
+     * @dev Emitted when a user withdraws tokens
+     * @param sender User address
+     * @param amount The amount of deposited tokens
+     * @param customId Custom identifier (for exchanges)
+     */
     event Withdrawn(address indexed sender, uint256 amount, string customId);
 
     uint256 constant YEAR = 365 days;
     address constant BURN_ADDRESS = 0x0000000000000000000000000000000000000001;
 
+    // STAKE token
     IERC20Mintable public token;
 
+    // The array of staking intervals
     uint256[] intervals;
+    // The array of interest rates for each staking interval
     uint256[] interestRates;
-    uint256 public fee; // in percentage, represented as fixed point numbers with 18 decimals like in Ether
-    uint256 public withdrawalLockDuration; // in seconds
+    // The fee of the forced withdrawal (in percentage)
+    uint256 public fee;
+    // The time from the request after which the withdrawal will be available (in seconds)
+    uint256 public withdrawalLockDuration;
 
+    // The deposit balances of users
     mapping (bytes32 => uint256) balances;
+    // The dates of users' deposits
     mapping (bytes32 => uint256) depositDates;
+    // The dates of users' withdrawal requests
     mapping (bytes32 => uint256) withdrawalRequestsDates;
 
+    // Variable that prevents reentrance
     bool locked;
 
+    /**
+     * @dev Initializes the contract
+     * @param _owner The owner of the contract
+     * @param _tokenAddress The address of the STAKE token contract
+     * @param _intervals The array of staking intervals
+     * @param _interestRates The array of interest rates for each staking interval
+     * @param _fee The fee of the forced withdrawal (in percentage)
+     * @param _withdrawalLockDuration The time from the request after which the withdrawal will be available (in seconds)
+     */
     function initialize(
         address _owner,
         address _tokenAddress,
@@ -47,6 +85,12 @@ contract EasyStaking is Ownable {
         _setWithdrawalLockDuration(_withdrawalLockDuration);
     }
 
+    /**
+     * @dev This method is used to deposit tokens.
+     * It calls the internal "_deposit" method and transfer tokens from user to contract
+     * @param _amount The amount to deposit
+     * @param _customId Custom identifier (for exchanges)
+     */
     function deposit(uint256 _amount, string calldata _customId) external {
         _deposit(msg.sender, _amount, _customId);
         _setLocked(true);
@@ -54,6 +98,12 @@ contract EasyStaking is Ownable {
         _setLocked(false);
     }
 
+    /**
+     * @dev This method is called when tokens are transferred to this contract
+     * using "transfer" method of STAKE token contract. It calls the internal "_deposit" method
+     * @param _sender The sender of tokens
+     * @param _amount The transferred amount
+     */
     function onTokenTransfer(address _sender, uint256 _amount, bytes calldata) external {
         require(msg.sender == address(token), "only token contract is allowed");
         if (!locked) {
@@ -61,16 +111,33 @@ contract EasyStaking is Ownable {
         }
     }
 
+    /**
+     * @dev This method is used to make a forced withdrawal with fee.
+     * It calls the internal "_withdraw" method
+     * @param _amount The amount to withdraw (0 - to withdraw all)
+     * @param _customId Custom identifier (for exchanges)
+     */
     function makeForcedWithdrawal(uint256 _amount, string calldata _customId) external {
         _withdraw(msg.sender, _amount, _customId, true);
     }
 
+    /**
+     * @dev This method is used to request a withdrawal without fee.
+     * It sets the date of the request
+     * @param _customId Custom identifier (for exchanges)
+     */
     function requestWithdrawal(string calldata _customId) external {
         bytes32 userHash = _getUserHash(msg.sender, _customId);
         // solium-disable-next-line security/no-block-members
         withdrawalRequestsDates[userHash] = block.timestamp;
     }
 
+    /**
+     * @dev This method is used to execute a requested withdrawal.
+     * It calls the internal "_withdraw" method and resets the date of the request
+     * @param _amount The amount to withdraw (0 - to withdraw all)
+     * @param _customId Custom identifier (for exchanges)
+     */
     function executeWithdrawal(uint256 _amount, string calldata _customId) external {
         bytes32 userHash = _getUserHash(msg.sender, _customId);
         uint256 requestDate = withdrawalRequestsDates[userHash];
@@ -84,6 +151,12 @@ contract EasyStaking is Ownable {
         withdrawalRequestsDates[userHash] = 0;
     }
 
+    /**
+     * @dev This method is used to claim unsupported tokens accidentally sent to the contract.
+     * It can be called only by owner
+     * @param _token The address of the token contract (zero address for native tokens)
+     * @param _to The address of the tokens receiver
+     */
     function claimTokens(address _token, address payable _to) public onlyOwner {
         require(_to != address(0) && _to != address(this), "not a valid recipient");
         if (_token == address(0)) {
@@ -98,10 +171,19 @@ contract EasyStaking is Ownable {
         }
     }
 
+    /**
+     * @dev Sets the staking token address. Can be called only by owner
+     * @param _tokenAddress The new address of the token
+     */
     function setToken(address _tokenAddress) external onlyOwner {
         _setToken(_tokenAddress);
     }
 
+    /**
+     * @dev Sets staking intervals and interest rates. Can be called only by owner
+     * @param _intervals The array of staking intervals
+     * @param _interestRates The array of interest rates for each staking interval
+     */
     function setIntervalsAndInterestRates(
         uint256[] calldata _intervals,
         uint256[] calldata _interestRates
@@ -109,37 +191,73 @@ contract EasyStaking is Ownable {
         _setIntervalsAndInterestRates(_intervals, _interestRates);
     }
 
+    /**
+     * @dev Sets the fee of the forced withdrawals. Can be called only by owner
+     * @param _fee The new fee value (in percentage)
+     */
     function setFee(uint256 _fee) external onlyOwner {
         _setFee(_fee);
     }
 
+    /**
+     * @dev Sets the time from the request after which the withdrawal will be available.
+     * Can be called only by owner
+     * @param _withdrawalLockDuration The new duration value (in seconds)
+     */
     function setWithdrawalLockDuration(uint256 _withdrawalLockDuration) external onlyOwner {
         _setWithdrawalLockDuration(_withdrawalLockDuration);
     }
 
+    /**
+     * @param _user The address of the user
+     * @param _customId Custom identifier (for exchanges)
+     * @return The deposit balance of the user
+     */
     function getBalance(address _user, string calldata _customId) external view returns (uint256) {
         bytes32 userHash = _getUserHash(_user, _customId);
         return balances[userHash];
     }
 
+    /**
+     * @param _user The address of the user
+     * @param _customId Custom identifier (for exchanges)
+     * @return The deposit date of the user
+     */
     function getDepositDate(address _user, string calldata _customId) external view returns (uint256) {
         bytes32 userHash = _getUserHash(_user, _customId);
         return depositDates[userHash];
     }
 
+    /**
+     * @param _user The address of the user
+     * @param _customId Custom identifier (for exchanges)
+     * @return The date of user's withdrawal request
+     */
     function getWithdrawalRequestDate(address _user, string calldata _customId) external view returns (uint256) {
         bytes32 userHash = _getUserHash(_user, _customId);
         return withdrawalRequestsDates[userHash];
     }
 
+    /**
+     * @return The array of staking intervals
+     */
     function getIntervals() external view returns (uint256[] memory) {
         return intervals;
     }
 
+    /**
+     * @return The array of interest rates
+     */
     function getInterestRates() external view returns (uint256[] memory) {
         return interestRates;
     }
 
+    /**
+     * @dev Calls internal "_mint" method and increases the user balance
+     * @param _sender The address of the sender
+     * @param _amount The amount to deposit
+     * @param _customId Custom identifier (for exchanges)
+     */
     function _deposit(address _sender, uint256 _amount, string memory _customId) internal {
         bytes32 userHash = _getUserHash(_sender, _customId);
         _mint(userHash);
@@ -147,6 +265,13 @@ contract EasyStaking is Ownable {
         emit Deposited(_sender, _amount, _customId);
     }
 
+    /**
+     * @dev Calls internal "_mint" method and then transfers tokens to the sender
+     * @param _sender The address of the sender
+     * @param _amount The amount to withdraw
+     * @param _customId Custom identifier (for exchanges)
+     * @param _forced With or without commission
+     */
     function _withdraw(address _sender, uint256 _amount, string memory _customId, bool _forced) internal {
         bytes32 userHash = _getUserHash(_sender, _customId);
         require(balances[userHash] > 0, "zero balance");
@@ -165,6 +290,10 @@ contract EasyStaking is Ownable {
         emit Withdrawn(_sender, amount, _customId);
     }
 
+    /**
+     * @dev Mints the user's interest and updates the deposit date
+     * @param _user The hash of the user
+     */
     function _mint(bytes32 _user) internal {
         // solium-disable-next-line security/no-block-members
         uint256 timePassed = block.timestamp.sub(depositDates[_user]);
@@ -182,11 +311,20 @@ contract EasyStaking is Ownable {
         depositDates[_user] = block.timestamp;
     }
 
+    /**
+     * @dev Sets the staking token address
+     * @param _tokenAddress The new address of the token
+     */
     function _setToken(address _tokenAddress) internal {
         require(_tokenAddress.isContract(), "not a contract address");
         token = IERC20Mintable(_tokenAddress);
     }
 
+    /**
+     * @dev Sets staking intervals and interest rates
+     * @param _intervals The array of staking intervals
+     * @param _interestRates The array of interest rates for each staking interval
+     */
     function _setIntervalsAndInterestRates(
         uint256[] memory _intervals,
         uint256[] memory _interestRates
@@ -197,19 +335,35 @@ contract EasyStaking is Ownable {
         interestRates = _interestRates;
     }
 
+    /**
+     * @dev Sets the fee of the forced withdrawals
+     * @param _fee The new fee value (in percentage)
+     */
     function _setFee(uint256 _fee) internal {
         require(_fee <= 1 ether, "should be less than or equal to 1 ether");
         fee = _fee;
     }
 
+    /**
+     * @dev Sets the time from the request after which the withdrawal will be available.
+     * @param _withdrawalLockDuration The new duration value (in seconds)
+     */
     function _setWithdrawalLockDuration(uint256 _withdrawalLockDuration) internal {
         withdrawalLockDuration = _withdrawalLockDuration;
     }
 
+    /**
+     * @dev Sets lock to prevent reentrance
+     */
     function _setLocked(bool _locked) internal {
         locked = _locked;
     }
 
+    /**
+     * @param _sender The address of the sender
+     * @param _customId Custom identifier (for exchanges)
+     * @return The unique hash of the user
+     */
     function _getUserHash(address _sender, string memory _customId) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(_sender, _customId));
     }
