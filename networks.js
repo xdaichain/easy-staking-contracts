451684ec36e07384683eefe95db11be10cd0718d
Max Alekseenko <nnwen0609@gmail.com>
2020-05-28 14:59:02 +0300

    add kovan network

--- networks.js
+++ networks.js
@@ -1,3 +1,11 @@
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
+
+function getProvider(network) {
+  return () => new HDWalletProvider(process.env.MNEMONIC, `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+}
+
 module.exports = {
   networks: {
     development: {
@@ -7,6 +15,11 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5e9,
       networkId: '*',
+    },
+    kovan: {
+      provider: getProvider('kovan'),
+      gasPrice: 1e10,
+      networkId: 42,
     },
   },
 };
