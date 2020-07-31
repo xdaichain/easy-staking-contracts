538fe417b74dd650eff2172ad3ce4567527f53e2
Max Alekseenko <nnwen0609@gmail.com>
2020-07-31 15:18:44 +0300

    small refactoring

--- contracts/EasyStaking.sol
+++ contracts/EasyStaking.sol
@@ -373,15 +373,14 @@ contract EasyStaking is Ownable {
     function setLiquidityProvidersRewardAddress(address _address) public onlyOwner {
         require(_address != address(0), "zero address");
         require(_address != address(this), "wrong address");
-        uint256 currentTimestamp = _now();
         AddressParam memory param = liquidityProvidersRewardAddressParam;
         if (param.timestamp == 0) {
             param.oldValue = _address;
-        } else if (currentTimestamp > param.timestamp.add(PARAM_UPDATE_DELAY)) {
+        } else if (_paramUpdateDelayElapsed(param.timestamp)) {
             param.oldValue = param.newValue;
         }
         param.newValue = _address;
-        param.timestamp = currentTimestamp;
+        param.timestamp = _now();
         liquidityProvidersRewardAddressParam = param;
         emit LiquidityProvidersRewardAddressSet(_address, msg.sender);
     }
@@ -418,10 +417,8 @@ contract EasyStaking is Ownable {
      * @return Returns current liquidity providers reward address.
      */
     function liquidityProvidersRewardAddress() public view returns (address) {
-        uint256 currentTimestamp = _now();
         AddressParam memory param = liquidityProvidersRewardAddressParam;
-        bool isUpdated = currentTimestamp > param.timestamp.add(PARAM_UPDATE_DELAY);
-        return isUpdated ? param.newValue : param.oldValue;
+        return _paramUpdateDelayElapsed(param.timestamp) ? param.newValue : param.oldValue;
     }
 
     /**
@@ -529,22 +526,27 @@ contract EasyStaking is Ownable {
      * @dev Sets the next value of the parameter and the timestamp of this setting.
      */
     function _updateUintParam(UintParam storage _param, uint256 _newValue) internal {
-        uint256 currentTimestamp = _now();
         if (_param.timestamp == 0) {
             _param.oldValue = _newValue;
-        } else if (currentTimestamp > _param.timestamp.add(PARAM_UPDATE_DELAY)) {
+        } else if (_paramUpdateDelayElapsed(_param.timestamp)) {
             _param.oldValue = _param.newValue;
         }
         _param.newValue = _newValue;
-        _param.timestamp = currentTimestamp;
+        _param.timestamp = _now();
     }
 
     /**
      * @return Returns the current value of the parameter.
      */
     function _getUintParamValue(UintParam memory _param) internal view returns (uint256) {
-        bool isUpdated = _now() > _param.timestamp.add(PARAM_UPDATE_DELAY);
-        return isUpdated ? _param.newValue : _param.oldValue;
+        return _paramUpdateDelayElapsed(_param.timestamp) ? _param.newValue : _param.oldValue;
+    }
+
+    /**
+     * @return Returns true if param update delay elapsed.
+     */
+    function _paramUpdateDelayElapsed(uint256 _paramTimestamp) internal view returns (bool) {
+        return _now() > _paramTimestamp.add(PARAM_UPDATE_DELAY);
     }
 
     /**
